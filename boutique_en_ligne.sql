1c9213127220f29164d18d4eb0c99637f951535b
HEUREUX Axel <axel.heureux@laplateforme.io>
2025-04-30 13:47:46 +0200

    Mise a jour de la BDD

--- boutique_en_ligne.sql
+++ boutique_en_ligne.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 30 avr. 2025 à 08:20
+-- Généré le : mer. 30 avr. 2025 à 11:47
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -124,19 +124,7 @@ INSERT INTO `produit` (`id_produit`, `nom`, `description`, `prix`, `image`, `sto
 (9, 'Coffret Cadeau Femme', 'Un coffret contenant un parfum et une crème.', 89.99, 'coffret_cadeau_femme.jpg', 20, '2025-04-30 09:33:35', 5),
 (10, 'Coffret Luxe Femme', 'Un coffret de luxe avec parfum et accessoires.', 129.99, 'coffret_luxe_femme.jpg', 10, '2025-04-30 09:33:35', 5),
 (11, 'Coffret Parfum Enfant', 'Un coffret contenant un parfum et un jouet.', 49.99, 'coffret_parfum_enfant.jpg', 30, '2025-04-30 09:33:35', 6),
-(12, 'Coffret Cadeau Enfant', 'Un coffret amusant avec parfum et accessoires.', 59.99, 'coffret_cadeau_enfant.jpg', 25, '2025-04-30 09:33:35', 6),
-(13, 'Eau de Toilette Homme', 'Un parfum frais et boisé pour hommes.', 49.99, 'eau_de_toilette_homme.jpg', 50, '2025-04-30 09:33:35', 1),
-(14, 'Parfum Intense Homme', 'Un parfum intense et sophistiqué pour hommes.', 69.99, 'parfum_intense_homme.jpg', 30, '2025-04-30 09:33:35', 1),
-(15, 'Eau de Parfum Femme', 'Un parfum floral et élégant pour femmes.', 59.99, 'eau_de_parfum_femme.jpg', 40, '2025-04-30 09:33:35', 2),
-(16, 'Parfum Luxe Femme', 'Un parfum de luxe avec des notes fruitées.', 89.99, 'parfum_luxe_femme.jpg', 20, '2025-04-30 09:33:35', 2),
-(17, 'Eau de Cologne Enfant', 'Un parfum doux et léger pour enfants.', 29.99, 'eau_de_cologne_enfant.jpg', 60, '2025-04-30 09:33:35', 3),
-(18, 'Parfum Fruité Enfant', 'Un parfum fruité et amusant pour enfants.', 34.99, 'parfum_fruite_enfant.jpg', 50, '2025-04-30 09:33:35', 3),
-(19, 'Coffret Cadeau Homme', 'Un coffret contenant un parfum et un gel douche.', 79.99, 'coffret_cadeau_homme.jpg', 25, '2025-04-30 09:33:35', 4),
-(20, 'Coffret Luxe Homme', 'Un coffret de luxe avec parfum et accessoires.', 119.99, 'coffret_luxe_homme.jpg', 15, '2025-04-30 09:33:35', 4),
-(21, 'Coffret Cadeau Femme', 'Un coffret contenant un parfum et une crème.', 89.99, 'coffret_cadeau_femme.jpg', 20, '2025-04-30 09:33:35', 5),
-(22, 'Coffret Luxe Femme', 'Un coffret de luxe avec parfum et accessoires.', 129.99, 'coffret_luxe_femme.jpg', 10, '2025-04-30 09:33:35', 5),
-(23, 'Coffret Parfum Enfant', 'Un coffret contenant un parfum et un jouet.', 49.99, 'coffret_parfum_enfant.jpg', 30, '2025-04-30 09:33:35', 6),
-(24, 'Coffret Cadeau Enfant', 'Un coffret amusant avec parfum et accessoires.', 59.99, 'coffret_cadeau_enfant.jpg', 25, '2025-04-30 09:33:35', 6);
+(12, 'Coffret Cadeau Enfant', 'Un coffret amusant avec parfum et accessoires.', 59.99, 'coffret_cadeau_enfant.jpg', 25, '2025-04-30 09:33:35', 6);
 
 -- --------------------------------------------------------
 
@@ -189,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `nom`, `prenom`, `email`, `mot_de_passe`, `adresse`, `role`) VALUES
-(3, 'HEUREUX', 'AXEL', 'axel-heureux@laplateforme.io', '$2y$10$vyq6rr7NsL9E.TBSVMjweOT5pfi8Yg9GrPA5InZrBv/FKHaE3yUbO', 'CHEMIN DE LA CROIX ISTRES 13800', 'client'),
+(3, 'HEUREUX', 'AXEL', 'axel-heureux@laplateforme.io', '$2y$10$vyq6rr7NsL9E.TBSVMjweOT5pfi8Yg9GrPA5InZrBv/FKHaE3yUbO', 'CHEMIN DE LA CROIX MARTIGUES 13800', 'client'),
 (4, 'MOGROVEJO', 'Justin', 'justin-morgrovejo@laplateforme.io', '$2y$10$Lvk9PzPMsC0WFUMW/QPELelr8bnmfJ5HypUKhqJoGUh1.QMDgAvs6', 'ISTRES 13800', 'client');
 
 --
